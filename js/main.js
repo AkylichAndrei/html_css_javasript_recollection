fe90bd54d9412aed93f8f477e6aca2a70134cdd5
AkylichAndrei <komlik_andrei@mail.ru>
2024-11-11 22:06:39 +0300

    add js code

--- js/main.js
+++ js/main.js
@@ -7,3 +7,48 @@ function summa(a,  b){
     info(result);
 }
 summa(5, 7);
+function onClickButton(el){
+    el.innerHTML = "You gave the button a press";
+}
+function onInput(el){
+    console.log(el.value);
+
+}
+function onMouseOver(el){
+    el.style.cssText = "width: 75px; height: 75px; background: green";
+}
+function onMouseOut(el){
+    el.style.background = "red";
+    el.style.cssText = "width: 50px; height: 50px;";
+}
+var text = document.getElementById('header');
+console.log(text.innerHTML);
+
+var span = document.getElementsByTagName('span');
+for(var i = 0 ; i < span.length; i++){
+    console.log(span[i].innerHTML);
+}
+
+function checkForm(el){
+   // var name = document.getElementById('name').value;
+    ///console.log(name);
+    var name = el.name.value;
+    console.log(name);
+    var err = "";
+   if(name == "")
+    err = "Введите имя";
+   else if(name.length <= 1)
+     err = "Введите значение больше 1";
+   
+
+if(err != ""){
+   document.getElementById('error').innerHTML = err;
+   
+}
+else{
+    alert("Все данные введены коректно");
+    window.location = 'https://wooordhunt.ru/';
+}
+
+    return false;
+}
